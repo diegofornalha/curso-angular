8f6a0d8090103dd28cf1f8e0a1cd95dc82856af5
Diego Fornalha <37958057+diegofornalha@users.noreply.github.com>
2021-12-13 20:25:07 -0300

    aula-11-v3
    simplificando e modificando o codigo para absorver o conhecimento

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,41 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
- styles: [
-    `
-      .highlight {
-          background-color: yellow;
-          font-weight: bold;
-      }
-    `
-  ]
+
 })
 export class DataBindingComponent implements OnInit {
 
-
-  valorAtual = '';
-  valorSalvo = '';
-
-  isMouseOver = true;
-
-  botaoClicado() {
-    alert('Botão clicado!');
-  }
+  valorAtual = 'Reprodução:';
 
   onKeyUp(evento: KeyboardEvent) {
     this.valorAtual = (<HTMLInputElement>evento.target).value;
-  }
-
-  salvarValor(valor: string) {
-    this.valorSalvo = valor;
-  }
-
-  onMouseOverOut() {
-    this.isMouseOver = !this.isMouseOver;
-  }
-
-  onMudouValor(evento: { novoValor: any; }) {
-    console.log(evento.novoValor);
   }
 
   constructor() { }
